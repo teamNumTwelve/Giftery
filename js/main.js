c68551698094968538a7ef8c61755d157d316531
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2020-06-15 23:46:14 +0200

    do the login proccess

--- js/main.js
+++ js/main.js
@@ -160,3 +160,150 @@ $("#star-rate").on("change",function(){
     $("#star-rate").addClass("is-valid");
   }
 });
+
+/* Login page */
+$("#userPass").on("change",function(){
+  const rate = $("#userPass").val();
+  if(validator.isEmpty(rate)){
+    $("#userPass").addClass("is-invalid");
+    return false;
+  } else{
+    $("#userPass").removeClass("is-invalid");
+    $("#userPass").addClass("is-valid");
+  }
+});
+
+$("#userEmail").on("change",function(){
+  const rate = $("#userEmail").val();
+  if(!validator.isEmail(rate)){
+    $("#userEmail").addClass("is-invalid");
+    $("#hintMessage").html("Your value <strong> "+rate+" </strong> is not email");
+    $('#myModal').modal('show');
+    return false;
+  } else{
+    $("#userEmail").removeClass("is-invalid");
+    $("#userEmail").addClass("is-valid");
+  }
+});
+
+$("#loginSubmit").on("click",function(){
+  const pass = $("#userPass").val();
+  const useremail = $("#userEmail").val();
+
+  if(validator.isEmpty(useremail) && validator.isEmpty(pass)){
+    $("#userEmail").addClass("is-invalid");
+    $("#userPass").addClass("is-invalid");
+    return false;
+  }
+
+  if(validator.isEmpty(pass)){
+    $("#userPass").addClass("is-invalid");
+    return false;
+  } else{
+    $("#userPass").removeClass("is-invalid");
+    $("#userPass").addClass("is-valid");
+  }
+
+  if(validator.isEmpty(useremail) || !validator.isEmail(message)){
+    $("#userEmail").addClass("is-invalid");
+    return false;
+  } else{
+    $("#userEmail").removeClass("is-invalid");
+    $("#userEmail").addClass("is-valid");
+  }
+  return;
+});
+/*log out */
+
+$("#userName").on("change",function(){
+  const userName = $("#userName").val();
+  if(validator.isEmpty(userName) || !validator.isAlphanumeric(userName,['en-US'])){
+    $("#userName").addClass("is-invalid");
+    $("#hintMessage").html("Your value <strong> "+userName+" </strong> is rowng it must not include spaces");
+    $('#myModal').modal('show');
+    return false;
+  } else{
+    $("#userName").removeClass("is-invalid");
+    $("#userName").addClass("is-valid");
+  }
+});
+
+$("#sigUserPass").on("change",function(){
+  const rate = $("#sigUserPass").val();
+  if(validator.isEmpty(rate)){
+    $("#sigUserPass").addClass("is-invalid");
+    return false;
+  } else{
+    $("#sigUserPass").removeClass("is-invalid");
+    $("#sigUserPass").addClass("is-valid");
+  }
+});
+
+$("#sigUserRePass").on("change",function(){
+  const pass = $("#sigUserPass").val();
+  const rePass = $("#sigUserRePass").val();
+  if(validator.isEmpty(rePass)){
+    $("#sigUserRePass").addClass("is-invalid");
+    $("#hintMessage").html("Your password is empty we can't accept this.");
+    $('#myModal').modal('show');
+    return false;
+  } else if(pass !== rePass){
+    $("#hintMessage").html("Your password is not match");
+    $('#myModal').modal('show');
+    $("#sigUserRePass").addClass("is-invalid");
+    return false;
+  }else{
+    $("#sigUserRePass").removeClass("is-invalid");
+    $("#sigUserRePass").addClass("is-valid");
+  }
+});
+
+$("#signSubmit").on("click",function(){
+  const pass = $("#sigUserPass").val();
+  const useremail = $("#userEmail").val();
+  const userName = $("#userName").val();
+  const rePass = $("#sigUserRePass").val();
+
+  if(validator.isEmpty(useremail) && validator.isEmpty(pass) && validator.isEmpty(rePass) && validator.isEmpty(userName)){
+    $("#userEmail").addClass("is-invalid");
+    $("#sigUserPass").addClass("is-invalid");
+    $("#userName").addClass("is-invalid");
+    $("#sigUserRePass").addClass("is-invalid");
+    return false;
+  }
+  else{
+    if(validator.isEmpty(userName) || !validator.isAlphanumeric(userName,['en-US'])){
+      $("#userName").addClass("is-invalid");
+      return false;
+    }
+    else{
+      $("#userName").removeClass("is-invalid");
+      $("#userName").addClass("is-valid");
+    }
+  
+    if(validator.isEmpty(useremail) || !validator.isEmail(message)){
+      $("#userEmail").addClass("is-invalid");
+      return false;
+    } else{
+      $("#userEmail").removeClass("is-invalid");
+      $("#userEmail").addClass("is-valid");
+    }
+  
+    if(validator.isEmpty(pass)){
+      $("#sigUserPass").addClass("is-invalid");
+      return false;
+    } else{
+      $("#sigUserPass").removeClass("is-invalid");
+      $("#sigUserPass").addClass("is-valid");
+    }
+  
+    if(validator.isEmpty(rePass) || pass !== rePass){
+      $("#sigUserRePass").addClass("is-invalid");
+      return false;
+    } else{
+      $("#sigUserRePass").removeClass("is-invalid");
+      $("#sigUserRePass").addClass("is-valid");
+    }
+  }
+  return false;
+});
